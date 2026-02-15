5f2ace32273862ad2cb951868478e4f2518bc7b3
Sanket <sanketsdeore@gmail.com>
2026-02-15 19:00:23 +0530

    Updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://alumni-connect-web.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
